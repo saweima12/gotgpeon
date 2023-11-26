c7d269d845f1c4bbf261219dc5319d9983f070d3
saweima12 <saweima12@gmail.com>
2023-11-26 23:35:07 +0800

    change whitelist -> allowlist

--- data/models/messageContext.go
+++ data/models/messageContext.go
@@ -10,7 +10,7 @@ type MessageContext struct {
 	ChatCfg     *ChatConfig          `json:"chat_cfg"`
 	CommonCfg   *config.CommonConfig `json:"common_cfg"`
 	Message     *core.MessageHelper  `json:"message"`
-	IsWhitelist bool                 `json:"is_whitelist"`
+	IsAllowlist bool                 `json:"is_allowlist`
 	Record      *MessageRecord       `json:"record"`
 }
 
